857971be41a080fcd1b1db66a57bd95f4f555caf
Johanos <johanos@bu.edu>
2019-03-06 23:54:06 -0500

    Adding files to have a test bed

---
+++ OpenCVUnityTestBed/OpenCVUnityTestBed.cpp
@@ -0,0 +1,14 @@
+// OpenCVUnityTestBed.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include "OpenCVUnity.h"
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+	testFunc();
+}
+
+
